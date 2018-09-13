9251864f5cf1379019c383f37dc167a01b783879
赵鹏 <zhaopeng@uzoo.cn>
2018-09-13 19:43:03 +0800

    临时更新

--- SQLtest/SQLtest.py
+++ SQLtest/SQLtest.py
@@ -1,30 +1,41 @@
-import codecs
-import os,sys
-import re
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-str = '你喜欢什么故事啊，能告诉我吗'
-# str = '你喜欢什么动画片'
-class Item:
-    def __init__(self, line):
-        self.content = line
-        self.userid = line.split()[11]
-        self.tag = str in line.split()[4]
+import time
+import pymysql
 
-dic = {}
+db = pymysql.connect('101.201.73.129', 'reader', '8TDURgAtWcYU6rTm', 'turing_platform_log', 3318, charset="utf8")
 
-f_in = codecs.open('shijianduan.txt', 'r', 'utf8')
-out = codecs.open('out.txt', 'w', 'utf8')
-
-for line in f_in.readlines():
+cursor = db.cursor()
+sql_select = 'SELECT * FROM platform_cloud_log_201805 WHERE apikey IN ("108bd270ef2d489f8bf714fde5ece5fc","aecdc3aa66824f6a9c84ac1b1531e698","05e7822d0cf14a22bd7aa37deafe7b60","3c03046e3bdd4594bf9f4b4337575f50","c099c14ffcbe4cc9ae5b0797e0555209","57e62a88572e4d4e811325c1bbce35b5","573f9f6108ef44f5a4f8f31110c5bf73")AND((create_date >= "2018-05-23 12:00:00" AND create_date <= "2018-05-23 13:59:59")) ORDER BY apikey LIMIT 10'
+data = cursor.execute(sql_select.encode('utf-8'))
+results = cursor.fetchall()
+f_out = open("result.txt", "w", encoding="utf-8")
+for row in results:
+    f_out.write(str(row[0]) + "\t")
+    f_out.write(row[1] + "\t")
+    f_out.write(row[2] + "\t")
+    f_out.write(row[3] + "\t")
+    f_out.write(row[4] + "\t")
+    f_out.write(row[5] + "\t")
+    f_out.write(str(row[6]) + "\t")
+    f_out.write(str(row[7]) + "\t")
+    f_out.write(str(row[8]) + "\t")
+    f_out.write(str(row[9]) + "\t")
+    f_out.write(row[10] + "\t")
+    f_out.write(str(row[11]) + "\t")
+    f_out.write(str(row[12]) + "\t")
     try:
-        item = Item(line.strip())
-    except Exception:
-        print(line)
+        f_out.write(row[13] + "\t")
+    except:
+        f_out.write(" " + "\t")
+    try:
+        f_out.write(row[14] + "\t")
+    except:
+        f_out.write(" " + "\t")
+    f_out.write(row[15] + "\n")
 
-    if item.userid in dic.keys():
-        out.write(dic.pop(item.userid) + '\n' + item.content + '\n\n')
-    if item.tag == True:
-        dic[item.userid] = item.content
-f_in.close()
-out.close()
+f_out.close()
+
+
 
--- python_IO/excel_io.py
+++ python_IO/excel_io.py
@@ -8,7 +8,7 @@ import xlrd
 import xlwt
 
 # 读excel
-data = xlrd.open_workbook('test_read.xlsx')
+data = xlrd.open_workbook('zz-test_read.xlsx')
 table = data.sheet_by_name('Sheet1') # 通过名称获取sheet
 print(table.col_values(0)) # 读取整列
 
@@ -16,5 +16,5 @@ print(table.col_values(0)) # 读取整列
 workbook = xlwt.Workbook(encoding='ascii') # 创建excel
 worksheet = workbook.add_sheet('123') # 创建sheet
 worksheet.write(0, 0, label='Row 0, Column 0 Value') #(列数， 行数， 输入的内容)
-workbook.save('Excel_Workbook.xls') # 保存表格
+workbook.save('zz-Excel_Workbook.xls') # 保存表格
 
