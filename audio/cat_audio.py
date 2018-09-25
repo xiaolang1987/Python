e498d7691f54630d3e962bef9f1d4fe391208a1a
赵鹏 <zhaopeng@uzoo.cn>
2018-09-25 19:59:32 +0800

    日常更新

--- audio/cat_audio.py
+++ audio/cat_audio.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pydub import AudioSegment
+import os
+import re
+
+
+# 循环目录下所有文件
+for each in os.listdir('.'):
+    filename = re.findall(r"(.*?)\.mp3", each) # 取出.mp3后缀的文件名
+    if filename:
+        filename[0] += '.mp3'
+        mp3 = AudioSegment.from_mp3(filename[0]) # 打开mp3文件
+        mp3[17*1000+500:].export(filename[0], format="mp3") # 切割前17.5秒并覆盖保存
--- python_IO/excel_io.py
+++ python_IO/excel_io.py
@@ -2,19 +2,39 @@
 # -*- coding: utf-8 -*-
 # pip install xlrd  安装读模块
 # pip install xlwt 安装写模块
-# 参考 https://www.cnblogs.com/MrLJC/p/3715783.html
+# 参考    https://www.cnblogs.com/MrLJC/p/3715783.html
+#           https://www.cnblogs.com/linyfeng/p/7123423.html
 
 import xlrd
 import xlwt
 
 # 读excel
 data = xlrd.open_workbook('zz-test_read.xlsx')
+"""读取整列数据"""
 table = data.sheet_by_name('Sheet1') # 通过名称获取sheet
-print(table.col_values(0)) # 读取整列
-
+print(table.col_values(0)[1:]) # 读取整列
+for a in table.col_values(0)[1:]:
+    if a:
+        print(a)
+"""获取所有sheet"""
+print(data.sheet_names())
+"""获取sheet名字"""
+print(data.sheet_names()[1])
+"""获取单元格数据类型"""
+print(table.cell(0, 0).ctype) # ctype : 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+"""强制转换数字"""
+number = 6.0
+if number == int(number):
+    int(number)
+else:
+    number = number
+""""""
+""""""
+""""""
+""""""
+""""""
 # 写excel
-workbook = xlwt.Workbook(encoding='ascii') # 创建excel
-worksheet = workbook.add_sheet('123') # 创建sheet
-worksheet.write(0, 0, label='Row 0, Column 0 Value') #(列数， 行数， 输入的内容)
-workbook.save('zz-Excel_Workbook.xls') # 保存表格
-
+# workbook = xlwt.Workbook(encoding='ascii') # 创建excel
+# worksheet = workbook.add_sheet('123') # 创建sheet
+# worksheet.write(0, 0, label='Row 0, Column 0 Value') #(行数， 列数， 输入的内容)
+# workbook.save('zz-Excel_Workbook.xls') # 保存表格
