31de04c6ac92cd75a702770b27670aea92dc70dc
赵鹏 <zhaopeng@uzoo.cn>
2018-09-30 16:35:59 +0800

    日常更新

--- python_IO/excel_io.py
+++ python_IO/excel_io.py
@@ -10,18 +10,30 @@ import xlwt
 
 # 读excel
 data = xlrd.open_workbook('zz-test_read.xlsx')
-"""读取整列数据"""
+"""通过sheet名读取sheet"""
 table = data.sheet_by_name('Sheet1') # 通过名称获取sheet
-print(table.col_values(0)[1:]) # 读取整列
+
+"""读取整列数据"""
+print("整列数据", table.col_values(0)[1:]) # 读取整列
 for a in table.col_values(0)[1:]:
     if a:
-        print('逐行打印：' + str(a))
+        print('逐行打印整列：' + str(a))
+
+"""读取整行数据"""
+print(table.row_values(0)[1:])
+for b in table.row_values(0)[1:]:
+    if b:
+        print('逐行打印正行：' + str(b))
+
 """获取所有sheet"""
 print(data.sheet_names())
+
 """获取sheet名字"""
 print(data.sheet_names()[1])
+
 """获取单元格数据类型"""
 print(table.cell(0, 0).ctype) # ctype : 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+
 """强制转换数字"""
 for a in table.col_values(0)[1:]:
     if a:
