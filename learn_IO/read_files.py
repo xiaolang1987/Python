28a58476136957833a7e27f6c2bc8864edf36a73
zhaopeng <zhao19870908@qq.com>
2019-03-31 11:19:35 +0800

    读写相关

--- learn_IO/read_files.py
+++ learn_IO/read_files.py
@@ -4,8 +4,10 @@
 import os
 import audio
 
-allfile = []
+
+# 获取文件列表
 def getallfile(path):
+    allfile = []
     allfilelist = os.listdir(path)
     for file in allfilelist:
         filepath = os.path.join(path, file)
@@ -16,9 +18,26 @@ def getallfile(path):
     return allfile
 
 
-getallfile("C:\\zhaopeng\\01-items\\01-doc\\02-ASR\\音频文件-80k左右\\")
-f_out = open('base64_result.txt', 'w')
-for item in allfile:
-    print(item)
+# 读取多段内容并保存
+def read_content(files):
+    lines = open(files, "r", encoding="utf-8").readlines()
+    fr = open("result.txt", "w", encoding="utf-8")
+    start_line = []
+    end_line = []
+    line_num = 0
+    for line in lines:
+        if "qwerty" in line:
+            start_line.append(line_num)
+        if "the end" in line:
+            end_line.append(line_num)
+        line_num += 1
+    print(start_line, end_line)
+    for i in range(len(start_line)):
+        for j in lines[start_line[i]+1:end_line[i]]:
+            fr.write(j)
+
+
+if "__name__" == "__name__":
+    read_content("read.txt")
 
 
